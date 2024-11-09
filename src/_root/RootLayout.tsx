dbef724487fd4c65b3ee750ac96d001c5219df88
Indrajit Mali <142807827+Indrajit-Mali1@users.noreply.github.com>
2024-11-09 22:37:38 +0530

    Update RootLayout.tsx

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,20 +1,28 @@
-
-import Topbar from '@/components/shared/Topbar'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import { Outlet } from 'react-router-dom'
-import Bottombar from '@/components/shared/Bottombar'
+import { useUserContext } from '@/context/AuthContext';
+import { Outlet, Navigate } from 'react-router-dom';
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import Bottombar from '@/components/shared/Bottombar';
 
 const RootLayout = () => {
-  return (
-    <div className='w-full md:flex'>
-      <Topbar />
-      <LeftSidebar />
-      <section className='flex flex-1 h-full'>
-        <Outlet />
-      </section>
-      <Bottombar />
-    </div>
-  )
-}
+    const { isAuthenticated, isLoading } = useUserContext();
 
-export default RootLayout
+    // Display a loading screen while auth status is being determined
+    if (isLoading) return <div>Loading...</div>;
+
+    // Redirect to sign-in page if user is not authenticated
+    return isAuthenticated ? (
+        <div className="w-full md:flex">
+            <Topbar />
+            <LeftSidebar />
+            <section className="flex flex-1 h-full">
+                <Outlet />
+            </section>
+            <Bottombar />
+        </div>
+    ) : (
+        <Navigate to="/sign-in" />
+    );
+};
+
+export default RootLayout;
